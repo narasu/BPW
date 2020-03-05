4ce7b0556258a64fe613661cea79268d4cff6206
narasu <narasu@users.noreply.github.com>
2020-03-05 12:49:29 +0100

    ja kut

--- Assets/Scripts/Weapons/Pistol.cs
+++ Assets/Scripts/Weapons/Pistol.cs
@@ -7,7 +7,7 @@ public class Pistol : Weapon
     protected override void Update()
     {
         base.Update();
-        
+
         if (!isEmpty && Input.GetMouseButtonDown(0))
         {
             Shoot();
